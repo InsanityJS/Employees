b25d49108717ec209888051e7739b34b3e01fc45
InsanityJs <maks.naymenko@gmail.com>
2021-10-04 18:01:36 +0300

    Create page'View', maked logic for 'Edit', 'Add, 'Delete'

--- src/js/app.js
+++ src/js/app.js
@@ -1,61 +1,96 @@
 angular.module("employeesApp", ["ngMaterial"]).controller("AppCtrl", function ($scope, $mdDialog) {
     $scope.status = "  ";
     $scope.customFullscreen = false;
-    $scope.currentView = "add";
+    $scope.currentView = "employees";
 
     $scope.employees = [
         {
             id: 0,
-            name: "Freddie Mercury",
+            username: "Freddie Mercury",
             department: "Designer",
-            textarea: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            textarea: 'Freddie Mercury (born Farrokh Bulsara; 5 September 1946 – 24 November 1991)[2] was a British singer, songwriter, record producer, and lead vocalist of the rock band Queen. Regarded as one of the greatest singers in the history of rock music, he was known for his flamboyant stage persona and four-octave vocal range. Mercury defied the conventions of a rock frontman, with his highly theatrical style influencing the artistic direction of Queen.',
         },
         {
             id: 1,
-            name: "Roger Meddows Taylor",
+            username: "Roger Meddows Taylor",
             department: "Biologist",
-            textarea: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            textarea: 'Roger Meddows Taylor OBE (born 26 July 1949) is an English musician, singer-songwriter and multi-instrumentalist, best known as the drummer for the rock band Queen.[1] As a drummer, Taylor was recognised early in his career for his unique sound.[2] He was voted by radio listeners as the eighth-greatest drummer in classic rock music history in a poll conducted by Planet Rock in 2005.[3]',
         },
         {
             id: 2,
-            name: "John Deacon",
+            username: "John Deacon",
             department: "Electronic engineer",
-            textarea: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            textarea: "John Richard Deacon (born 19 August 1951) is an English retired musician, best known for being the bassist for the rock band Queen. He composed several songs for the group, including Top 10' hits 'You're My Best Friend', 'Another One Bites the Dust', and 'I Want to Break Free' co-wrote 'Under Pressure', 'Friends Will Be Friends' and 'One Vision', and he was involved in the bands financial management",
         },
         {
             id: 3,
-            name: "Brian May",
+            username: "Brian May",
             department: "Astrophysicist",
-            textarea: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            textarea: 'Brian Harold May CBE (born 19 July 1947) is an English musician, singer, songwriter, record producer, author, astrophysicist, and university administrator. He is the lead guitarist of the rock band Queen. May is widely regarded as one of the greatest guitarists of all time.',
         },
         {
             id: 4,
-            name: "John Peel",
+            username: "John Peel",
             department: "Journalist",
-            textarea: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            textarea: 'John Robert Parker Ravenscroft OBE (30 August 1939 – 25 October 2004), known professionally as John Peel, was an English disc jockey, radio presenter, record producer and journalist. He was the longest-serving of the original BBC Radio 1 DJs, broadcasting regularly from 1967 until his death in 2004.',
         },
     ];
 
     $scope.select = ["", "Designer", "Astrophysicist", "Biologist", "Electronic engineer", "Journalist"];
     $scope.selected = $scope.select[0];
 
+    $scope.addEmployee = function (item) {
+        $scope.data = item ? angular.copy(item) : {};
+        $scope.currentView = "add";
+     };
+    $scope.edit = function (item) {
+        $scope.data = item ? angular.copy(item) : {};
+        $scope.currentView = "add";
+
+    };
+
+    $scope.view = function (item) {
+        $scope.data = item ? angular.copy(item) : {};
+        $scope.currentView = "view";
+
+    };
+
+   
+    $scope.cancel = function () {
+        $scope.data = {};
+        $scope.currentView = "employees";
+        }; 
+
+    //! Save data
+    $scope.pushData = function (item) {
+        if (angular.isDefined(item.id)) {
+            for (var i = 0; i < $scope.employees.length; i++) {
+                if ($scope.employees[i].id == item.id) {
+                    $scope.employees[i] = item;
+                    break;
+                }
+            }
+            $scope.currentView = "employees"
+        } else {
+            $scope.employees.push(item);
+            $scope.currentView = "employees"
+        }
+        }
+
     $scope.showConfirm = function (ev) {
         var confirm = $mdDialog
             .confirm()
-            .title("Would you like to delete your debt?")
-            .textContent("All of the banks have agreed to forgive you your debts.")
+            .title("Do you want to delete an employee?")
+            .textContent("This an employee will be delete from database")
             .ariaLabel("Lucky day")
             .targetEvent(ev)
-            .ok("Please do it!")
-            .cancel("Sounds like a scam");
+            .ok("Delete!")
+            .cancel("Cancel");
 
-        $mdDialog.show(confirm).then(
-            function () {
-                $scope.status = "You decided to get rid of your debt.";
-            },
-            function () {
-                $scope.status = "You decided to keep your debt.";
-            }
-        );
+            $mdDialog.show(confirm).then(function (item) {
+                $scope.employees.splice($scope.employees.indexOf(item), 1);
+                }, function () {
+                return;
+              });
     };
 });
